3575f5e65a1328b9cbcc34dff14ce596f76f32d8
Artur Więcek <artur.wiecek@yahoo.com>
2020-06-07 09:30:30 +0200

    add project to git

---
+++ domki/domki/src/main/java/com/nwaproject/domki/DomkiApplication.java
@@ -0,0 +1,13 @@
+package com.nwaproject.domki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomkiApplication.class, args);
+	}
+
+}
---
+++ domki/domki/src/test/java/com/nwaproject/domki/DomkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwaproject.domki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
